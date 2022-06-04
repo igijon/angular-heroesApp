3045299d6405f95a48f37fced952d940ab17bf25
Inma Gijón <igijoninf@gmail.com>
2022-06-04 18:07:53 +0200

    Navlist

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatList, MatListModule} from '@angular/material/list';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
@@ -10,6 +11,7 @@ import {MatSidenavModule} from '@angular/material/sidenav';
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatToolbarModule,
     MatSidenavModule
   ]
