31a3190afb0fed6cbe9c86c4e960d25b4443b630
Inma Gijón <igijoninf@gmail.com>
2022-06-04 11:52:00 +0200

    Lazy load y rutas hijas: Héroes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ) //Clave del lazy load
   },
   {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then( m => m.HeroesModule ) 
+  },
+  {
     path: '404',
     component: ErrorPageComponent
   },
