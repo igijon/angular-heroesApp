4d3aeb12dab8bbba0f9456d836de93838340ecec
Inma Gijón <igijoninf@gmail.com>
2022-06-05 08:56:29 +0200

    FlexLayout Resoluciones

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -6,6 +6,11 @@ import { HeroesService } from '../../services/heroes.service';
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styles: [
+    `
+    mat-card {
+      margin-top: 20px
+    }
+    `
   ]
 })
 export class ListadoComponent implements OnInit {
