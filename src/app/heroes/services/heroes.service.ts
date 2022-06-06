db06828f30430273dc072da8f6e052d8f80a7748
Inma Gijón <igijoninf@gmail.com>
2022-06-06 18:37:16 +0200

    Variables de entorno para base URL del API

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,20 +3,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Heroe } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  private baseURL: string = environment.baseUrl;
+
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseURL}/heroes`);
   }
 
   getHeroePorId(id: string): Observable<Heroe>{
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseURL}/heroes/${id}`);
   }
 
 }
