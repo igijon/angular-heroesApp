3deae23d6e09ba06b488d27ff465ec6fcd700e16
Inma Gijón <igijoninf@gmail.com>
2022-06-06 18:15:27 +0200

    Obtener datos héroe del endpoint

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -9,10 +12,29 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  heroe!: Heroe;
+
+  constructor(private activatedRoute: ActivatedRoute, private heroesService: HeroesService) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(({id}) => console.log(id));
+    this.activatedRoute.params
+      .pipe(switchMap(({id}) => this.heroesService.getHeroePorId(id)))
+      .subscribe(
+        (heroe) => {
+          this.heroe = heroe;
+        }
+      );
+    // this.activatedRoute.params
+    //   .subscribe(
+    //     ({id}) =>
+    //     {
+    //       this.heroesService.getHeroePorId(id)
+    //         .subscribe(
+    //           (resp) => {
+    //             this.heroe = resp;
+    //           });
+    //     });
   }
+
 
 }
