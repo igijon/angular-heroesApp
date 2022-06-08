349c164bcffd6f0a228838485d3d1943daa99825
Inma Gijón <igijoninf@gmail.com>
2022-06-08 18:54:16 +0200

    Login básico

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,9 +14,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.router.navigate(['./auth']);
+  }
 }
