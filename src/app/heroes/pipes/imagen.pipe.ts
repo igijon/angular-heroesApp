e1014eb8e8a18b086fd1a26006c3ee6c79fd165c
Inma Gijón <igijoninf@gmail.com>
2022-06-08 17:47:44 +0200

    Pipe pure

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -2,12 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  //pure: false //Se disparará cada vez que el ciclo de detección de cambios se dispare. Con pure
+  //por defecto a true se disparará cuando cambie el objeto (no la propiedad)
 })
 export class ImagenPipe implements PipeTransform {
 
   imgBase:string = 'assets/heroes/';
   transform(value: Heroe): string {
+
     if(!value.id && !value.alt_img) {
       return  `assets/no-image.png`;
     }
