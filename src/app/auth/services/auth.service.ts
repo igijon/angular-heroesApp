57128a085c1b9828ad93d6ab8b35de57a4fe6abe
Inma Gijón <igijoninf@gmail.com>
2022-06-08 19:37:26 +0200

    AuthService: principios básicos

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { Auth } from '../interfaces/auth.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  login(): Observable<Auth> {
+    return this.http.get<Auth>(`${ this.baseUrl }/usuarios/1`);
+  }
+}
